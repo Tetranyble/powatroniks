fe82cdf7a1a8ff131f7b47eda043d40dfe3e04c8
Tetranyble <senenerst@gmail.com>
2019-11-29 14:48:15 +0100

    little addition to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Powatroniks Tech Hub
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/Tetranyble">Meet the developer </a>
+
+                    <a href="https://github.com/Tetranyble/powatroniks">Contribute on GitHub</a>
                 </div>
             </div>
         </div>
