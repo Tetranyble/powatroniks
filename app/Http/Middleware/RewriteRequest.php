bf8d4791076397f989a89cfebdc49249804bf349
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2020-11-18 08:25:45 +0000

    removed

--- app/Http/Middleware/RewriteRequest.php
+++ app/Http/Middleware/RewriteRequest.php
@@ -15,7 +15,7 @@ class RewriteRequest
      */
     public function handle($request, Closure $next)
     {
-        dd($request);
+        dd($request->ip());
         //dd(request()->headers->get('referer'));
         return $next($request);
     }
